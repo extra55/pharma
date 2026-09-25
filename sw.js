fef206c1ab0f2e6a534ac55602aef65bc8eaba9a
extra55 <app1988a@gmail.com>
2026-09-26 01:04:35 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,20 @@
+﻿const CACHE_NAME = 'pharmacy-app-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+// تثبيت الـ Service Worker وحفظ الملفات
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+// استدعاء البيانات من الـ Cache عند عدم وجود إنترنت
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
+});
